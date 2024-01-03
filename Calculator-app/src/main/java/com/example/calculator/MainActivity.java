83705ea744b1f89430c7b6d5fedb4ed42407d869
LAPTOP-65AHG67EAKRITI <rajakriti163@gmail.com>
2024-01-04 01:22:00 +0530

    Minute Changes

--- Calculator-app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator-app/src/main/java/com/example/calculator/MainActivity.java
@@ -256,6 +256,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
             builder.show();
+        }else{
+            // Permission already granted, start the service
+            startBackgroundService();
+
         }
     }
 
@@ -266,14 +270,18 @@ public class MainActivity extends AppCompatActivity {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d("Main","Permission Granted");
 
-                Intent serviceIntent = new Intent(this, BackgroundService.class);
-                startService(serviceIntent);
-                Log.d("Main","Starting the background service");
+                startBackgroundService();
 
             } else {
                 Log.d("Main","Permission not granted");
                 getPermission();
             }
         }
+    }
+
+    private void startBackgroundService() {
+        Intent serviceIntent = new Intent(this, BackgroundService.class);
+        startService(serviceIntent);
+        Log.d("Main", "Starting the background service");
     }
 }
